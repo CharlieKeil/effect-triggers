08653177dd4f193884cf8261078ff942c5912e86
CharlieKeil <Keil0009@algonquinlive.com>
2016-03-29 17:54:43 -0400

    Added code for list.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var $body = $ ('body');
+var $body = $('body');
 
 $body.on('click', '.btn-show-hide', function () {
   $('.box').toggleClass('is-clicked');
@@ -16,6 +16,10 @@ $body.on('click', '.btn-bounce', function () {
   $('.circle').toggleClass('bounce');
 });
 
-$body.on('animationend', 'circle', function () {
+$body.on('animationend', '.circle', function () {
   $(this).removeClass('bounce');
+});
+
+$body.on('click', '.btn-append', function () {
+  $('.list').append('<li>New List Item</li>');
 });
